ed8b89d57108ff9f2b4cffe5583b15c6b867586a
leandroBraz <ando1992>
2017-07-27 21:56:04 -0300

    Atualização da Persistencia  1.0 Atualização geral V 2.0

--- app/src/main/java/com/projeto/bookfast/bookfast/negocio/ValidarLogin.java
+++ app/src/main/java/com/projeto/bookfast/bookfast/negocio/ValidarLogin.java
@@ -1,8 +1,22 @@
 package com.projeto.bookfast.bookfast.negocio;
 
+import android.widget.EditText;
+
 /**
  * Created by oi on 27/07/2017.
  */
 
 public class ValidarLogin {
+    String senha = "2012";
+    String login =" 1234567890";
+
+    public boolean validarLogin (EditText usuarioCPF, EditText editSenha) {
+        if (usuarioCPF.equals(login) && editSenha.equals(senha)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
 }
