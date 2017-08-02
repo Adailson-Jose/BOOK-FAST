2eb59d0e270bc9e39a6b07bbd8d08b439a21745d
Adailson-Jose <adailsoncca@hotmail.com.br>
2017-08-02 18:38:46 -0300

    V2.5
    1-  Correção do user ADMINISTRADOR

--- app/src/main/java/com/projeto/bookfast/bookfast/dominio/pessoa/gui/TelaInicialUsuarioComum.java
+++ app/src/main/java/com/projeto/bookfast/bookfast/dominio/pessoa/gui/TelaInicialUsuarioComum.java
@@ -12,8 +12,6 @@ import com.projeto.bookfast.bookfast.dominio.livro.gui.TelaQRcode;
 import com.projeto.bookfast.bookfast.dominio.pessoa.dominio.Pessoa;
 import com.projeto.bookfast.bookfast.dominio.pessoa.percistencia.ReadPessoa;
 
-import java.util.Stack;
-
 public class TelaInicialUsuarioComum extends Activity {
     Pessoa pessoa;
     TextView textViewDados;
@@ -25,12 +23,12 @@ public class TelaInicialUsuarioComum extends Activity {
         setContentView(R.layout.activity_tela_inicial_usuario_comum);
         btEmprestimoQRcode = (Button) findViewById(R.id.btEmprestimoQRcode);
         ReadPessoa busca = new ReadPessoa(getApplicationContext());
-        textViewDados = (TextView) findViewById(R.id.textViewDados);
+        //textViewDados = (TextView) findViewById(R.id.textViewDados);
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             pessoa = busca.getPessoa(Long.parseLong(String.valueOf(bundle.get("KEY"))));
             String dadosPessoa = "Nome: " + pessoa.getNome() + ", Cpf: " + pessoa.getCpf() + ", Senha: " + pessoa.getSenha() + " Id: " + pessoa.getId() + ", Email: " + pessoa.getEmail() + ".";
-            textViewDados.setText(dadosPessoa);
+            //textViewDados.setText(dadosPessoa);
         } else {
             textViewDados.setText("UM ERRO OCORREU.");
         }
