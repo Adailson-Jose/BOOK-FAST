67e89bfdd43cf9b784bfc6edb4578d208ed64b87
Adailson-Jose <adailsoncca@hotmail.com.br>
2017-08-15 10:52:02 -0300

    V 2.7 Correção do erro que estava acontecendo, o erro era na criação das tabelas.

--- app/src/main/java/com/projeto/bookfast/bookfast/persistencia/CreatBancoDados.java
+++ app/src/main/java/com/projeto/bookfast/bookfast/persistencia/CreatBancoDados.java
@@ -73,9 +73,10 @@ public class CreatBancoDados extends SQLiteOpenHelper {
         db.execSQL("INSERT INTO " + TABELA_LIVRO + "(" + COLUNA_ISBN + "," + COLUNA_NOME_LIVRO + ","
                 + COLUNA_QTD_ALUGADO + "," + COLUNA_AUTOR + "," + COLUNA_GENERO + "," + COLUNA_QTD_TOTAL + "," + COLUNA_ANO + "," + COLUNA_N_EDICAO + "," + COLUNA_FOTO_LIVRO +
                 ") VALUES('9788502210455', 'ECONOMIA', '10', 'Paulo Vicecont', 'Educação', '50', '2017', '0','')");
-        db.execSQL("create table" + TABELA_ALUGUEL + "(" + COLUNA_ID_ALUGUEL + "integer primary key autoincrement, " + COLUNA_PESSOA_ALUGUEL + " integer)");
 
-        db.execSQL("creat table" + TABELA_ITEM_LIVRO + "("+ COLUNA_ID_ITEM + "integer," + COLUNA_ID_ITEM_ALUGUEL + "integer," +COLUNA_ID_ITEM_LIVRO + "integer)" );
+        db.execSQL("create table " + TABELA_ALUGUEL + "(" + COLUNA_ID_ALUGUEL + " integer primary key autoincrement, " + COLUNA_PESSOA_ALUGUEL + " integer)");
+
+        db.execSQL("create table " + TABELA_ITEM_LIVRO + "(" + COLUNA_ID_ITEM + " integer primary key autoincrement, " + COLUNA_ID_ITEM_ALUGUEL + " integer, " + COLUNA_ID_ITEM_LIVRO + " integer)");
 
     }
 
