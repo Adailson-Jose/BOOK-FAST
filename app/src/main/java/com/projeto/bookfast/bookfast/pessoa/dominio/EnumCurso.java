0e360c2e233d0ea7a7ce1f9738f9b35bfbf2abb3
Adailson-Jose <adailsoncca@hotmail.com.br>
2017-08-22 10:04:24 -0300

    Versão 4.7...
             Mehoramento do spinerCursos

--- app/src/main/java/com/projeto/bookfast/bookfast/pessoa/dominio/EnumCurso.java
+++ app/src/main/java/com/projeto/bookfast/bookfast/pessoa/dominio/EnumCurso.java
@@ -17,15 +17,11 @@ public enum EnumCurso {
         return descricao;
     }
 
-    public String capitalize() {
-        return this.toString().substring(0, 1) + this.toString().substring(1).toLowerCase();
-    }
-
     public static String[] listaEnumCurso() {
         EnumCurso[] listaCurso = EnumCurso.values();
         String[] lista = new String[listaCurso.length];
         for (int i = 0; i < listaCurso.length; i++) {
-            lista[i] = listaCurso[i].capitalize();
+            lista[i] = listaCurso[i].getDescricao();
         }
         return lista;
     }
